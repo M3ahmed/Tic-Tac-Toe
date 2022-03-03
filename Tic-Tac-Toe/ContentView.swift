835ef7bf16ca737f31b3dd75020af03ab4387d3c
Mohamed Ahmed <mohamedsaidahmed@outlook.com>
2022-03-02 16:31:18 -0800

    added few changes

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                                GridItem(.flexible()),]
     
     @State private var moves : [Move?] = Array(repeating: nil, count: 9)
-    
+    @State private var isGameBoardDisabled = false
     var body: some View {
         GeometryReader { geometry in
             VStack{
@@ -34,18 +34,27 @@ struct ContentView: View {
                         .onTapGesture {
                             if isSquareOccupied(in: moves, forIndex: i) {return}
                             moves[i] = Move(player: .human, boardIndex: i)
+                            isGameBoardDisabled = true
                             
                             // Check for win condition or draw
+                            if checkWinCondtion(for: .human, in: moves){
+                                print("Human Wins")
+                            }
                             
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
                                 let computerPosition = determineComputerMovePostion(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                isGameBoardDisabled = false
+                                if checkWinCondtion(for: .computer, in: moves){
+                                    print("Computer Wins")
+                                }
                             }
                         }
                     }
                 }
                 Spacer()
             }
+            .disabled(isGameBoardDisabled)
             .padding()
         }
     }
@@ -61,6 +70,16 @@ struct ContentView: View {
             movePosition = Int.random(in: 0..<9)
         }
         return movePosition
+    }
+    func checkWinCondtion(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        
+        let playerMoves = moves.compactMap {$0}.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions){ return true }
+        
+        return false
     }
 }
 
