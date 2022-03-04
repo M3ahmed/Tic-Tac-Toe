3d30c2b30f3fb1280bf8097114f04632d28343fb
Mohamed Ahmed <mohamedsaidahmed@outlook.com>
2022-03-03 17:28:52 -0800

    before the AI gets buffed

--- Tic-Tac-Toe/Alerts.swift
+++ Tic-Tac-Toe/Alerts.swift
@@ -16,13 +16,14 @@ struct AlertItem: Identifiable{
 }
 
 struct AlertContext {
-    let humanWin    = AlertItem(title: Text("You Win"),
-                                message: Text("You are so smart. You beat your own AI"),
-                                buttonTitle: Text("Hell Yeah!"))
-    let computerWin = AlertItem(title: Text("You Lost"),
-                                message: Text("You programmed a super AI"),
-                                buttonTitle: Text("Rematch"))
-    let draw        = AlertItem(title: Text("Draw"),
-                                message: Text("What a bottle of whits we have here..."),
-                                buttonTitle: Text("Try Again"))
+    
+    static let humanWin    = AlertItem(title: Text("You Win"),
+                                       message: Text("You are so smart. You beat your own AI"),
+                                       buttonTitle: Text("Hell Yeah!"))
+    static let computerWin = AlertItem(title: Text("You Lost"),
+                                       message: Text("You programmed a super AI"),
+                                       buttonTitle: Text("Rematch"))
+    static let draw        = AlertItem(title: Text("Draw"),
+                                       message: Text("What a bottle of whits we have here..."),
+                                       buttonTitle: Text("Try Again"))
 }
